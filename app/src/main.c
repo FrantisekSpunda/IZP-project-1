c44c3e75a7f089433d0417f7d914d3983d6aa77d
František Špunda <frantisek.spunda@navmatix.com>
2023-10-11 13:57:25 +0200

    fix casesencitive etc.

--- app/src/main.c
+++ app/src/main.c
@@ -10,64 +10,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "main.h"
 
-#define SIZE_OF_ARG 2
-
-int stringLength(char *str)
+void removeDiacritics(char *str)
 {
-  int length = 0;
-  while (str[length] != '\0')
+  for (int i = 0; i < 10; i++)
   {
-    length++;
+    str[i] = tolower(str[i]);
   }
-
-  return length;
 }
+
+void stringLowercase(char *string);
 
 int main(int argc, char *argv[])
 {
-  if (argc != 2)
+  char user_input[100] = "";
+  for (int i = 1; i < argc; i++)
   {
-    printf("test: %s --\n", argv[0]);
-    return 1;
+    strcat(user_input, argv[i]);
+    if (i < argc - 1)
+      strcat(user_input, " ");
   }
 
-  char *input = argv[1];
-  printf("User input is %s and  lenght is %i\n", input, stringLength(input));
+  char address[100];          // Variable for one line of file
+  char recomand[128] = "";    // Variable for recommended characters
+  int found = 0;              // Number of recomandations we found
+  char lastAddress[100] = ""; // Keep last address for printing single address
 
-  char town[100];          // One line of input file
-  char recomand[128] = ""; // Variable where will be first chars of towns
-  int foundedTowns = 0;    // If we can't find word, we have to print "Not found"
-
-  while (fgets(town, sizeof(town), stdin) != NULL)
+  while (fgets(address, sizeof(address), stdin) != NULL) // Get one line of file
   {
-    char isSimilar = 1; // If word is similar, we want to add char to recomand
+    char isSimilar = 1; // If word is similar, we want to add character to recomand
 
-    // For each letter in user input. If
-    for (int i = 0; i < stringLength(input); i++)
+    // Check if user input is similar to address
+    for (unsigned int i = 0; i < strlen(user_input); i++)
     {
-      if (input[i] != town[i])
+      if (tolower(user_input[i]) != tolower(address[i]))
       {
         isSimilar = 0;
         break;
       }
-
-      // printf("%c %c\n", input[i], town[i]); // Tisk znaků
     }
+
     if (isSimilar)
     {
-      printf("%s", town);
-      printf(" index %c \n", town[stringLength(input)]);
-      foundedTowns++;
-      recomand[stringLength(recomand)] = town[stringLength(input)];
+      // Save last address. If we will found one, we will print it
+      strcpy(lastAddress, address);
+      found++;
+
+      // Add character to recomand if it's not in recomand
+      if (!strrchr(recomand, toupper(address[strlen(user_input)])))
+        recomand[strlen(recomand)] = toupper(address[strlen(user_input)]);
     }
   }
 
-  if (foundedTowns)
+  if (found == 1)
   {
-    printf("Found %i\n", foundedTowns);
-    printf("Recomand: %s\n", recomand);
+    printf("\033[0;32m Found: \033[0m%s\n", lastAddress);
+  }
+  else if (found > 1)
+  {
+    printf("\033[0;34m Enable: \033[0m%s\n", recomand);
   }
   else
   {
@@ -75,4 +79,12 @@ int main(int argc, char *argv[])
   }
 
   return 0;
+}
+
+void stringLowercase(char *string)
+{
+  for (unsigned int i = 0; i < strlen(string); i++)
+  {
+    string[i] = tolower(string[i]);
+  }
 }
